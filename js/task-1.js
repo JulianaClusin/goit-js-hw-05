abf63dbb3e8f23e448f5a8d3212d1d7c08387c16
JulianaClusin <naymenko95@gmail.com>
2020-11-12 20:48:23 +0300

    adds homework

---
+++ js/task-1.js
@@ -0,0 +1,43 @@
+// Write code under this line
+const Account = function (login, email) {
+  this.login = login;
+  this.email = email;
+};
+Account.prototype.getInfo = function () {
+  return `login :${this.login}, email: ${this.email}`;
+};
+
+console.log(typeof Account.prototype.getInfo);
+// 'function'
+
+const mango = new Account("Mangozedog", "mango@dog.woof");
+console.log(mango.getInfo());
+// 'login : Mangozedog, email: mango@dog.wooaf'
+
+const poly = new Account("Poly", "poly@mail.com");
+console.log(poly.getInfo());
+// 'login : Poly, email: poly@mail.com'
+
+// Write code under this line
+// const Account = function ({ login, email }) {
+//   this.login = login;
+//   this.email = email;
+// };
+// Account.prototype.getInfo = function () {
+//   console.log(`Login: ${this.login}, Email: ${this.email}`);
+// };
+// console.log(typeof Account.prototype.getInfo);
+// // 'function'
+// const mango = new Account({
+//   login: "Mangozedog",
+//   email: "mango@dog.woof",
+// });
+
+// mango.getInfo();
+// // 'login : Mangozedog, email: mango@dog.woof'
+// const poly = new Account({
+//   login: "Poly",
+//   email: "poly@mail.com",
+// });
+// poly.getInfo();
+// 'login : Poly, email: poly@mail.com'
